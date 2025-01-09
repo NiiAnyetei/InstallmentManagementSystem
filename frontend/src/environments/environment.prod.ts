2fc46f18aaeda49a634ade2f4ffc4c9d6c2ad6bb
Clifford Anang <nunooclifford25@gmail.com>
2025-01-09 10:37:09 +0000

    adds prod config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: "https://cqh60c1l-7009.uks1.devtunnels.ms/api"
-  apiUrl: "https://localhost:7009/api"
+  apiUrl: "https://niianyetei-001-site1.qtempurl.com/api"
+  // apiUrl: "https://localhost:7009/api"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "https://cqh60c1l-7009.uks1.devtunnels.ms/api"
   apiUrl: "https://localhost:7009/api"
 };
 
